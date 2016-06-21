75c9852ffcf67b2d1fa892d9ea0ee000389a243d
emilycharris <emily.harris.mail@gmail.com>
2016-06-21 13:11:26 -0400

    end of day 2 coding

--- chirp/urls.py
+++ chirp/urls.py
@@ -15,10 +15,12 @@ Including another URLconf
 """
 from django.conf.urls import url, include
 from django.contrib import admin
-from main.views import IndexView
+from main.views import IndexView, ChirpDetailView, ChirpCreateView
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
     url(r'^$', IndexView.as_view(), name='index_view'),
-    url('^', include('django.contrib.auth.urls'))
+    url('^', include('django.contrib.auth.urls')),
+    url(r'^chirp/(?P<pk>\d+)/$', ChirpDetailView.as_view(), name='chirp_detail_view'),
+    url(r'^create_chirp/$', ChirpCreateView.as_view(), name='chirp_create_view')
 ]
--- main/views.py
+++ main/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from django.views.generic.base import TemplateView
-from django.views.generic import ListView
+from django.views.generic import ListView, DetailView
+from django.views.generic.edit import CreateView
 
 from main.models import Chirp
 
@@ -14,3 +15,24 @@ class IndexView(ListView):
         context = super().get_context_data(**kwargs) #you have to have this line
         context['amount'] = Chirp.objects.all().count()
         return context
+
+class ChirpDetailView(DetailView):
+    model = Chirp
+
+    #use for detail transactions in homework
+    def get_queryset(self):
+        return Chirp.objects.filter(bird=self.request.user) #do this for security
+
+class ChirpCreateView(CreateView):
+    model = Chirp
+    fields = ['body']
+    success_url = '/'
+
+    #to add authenticated user to chirp automatically
+    #creates instance of chirp without actually updating database
+
+    def form_valid(self, form):
+
+        chirp = form.save(commit=False)
+        chirp.bird = self.request.user
+        return super().form_valid(form)
