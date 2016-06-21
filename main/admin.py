bf561d9d0622061141c2bb6338706e0576e18c3f
emilycharris <emily.harris.mail@gmail.com>
2016-06-21 10:51:52 -0400

    created a list view for chirps

--- main/admin.py
+++ main/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
+from django.db import models
+from main.models import Chirp
 
 # Register your models here.
+
+class ChirpAdmin(admin.ModelAdmin):
+    list_display = ['body', 'bird']
+    search_fields = ['body']
+
+admin.site.register(Chirp, ChirpAdmin)
--- main/views.py
+++ main/views.py
@@ -1,8 +1,16 @@
 from django.shortcuts import render
 from django.views.generic.base import TemplateView
+from django.views.generic import ListView
+
+from main.models import Chirp
 
 # Create your views here.
 
-class IndexView(TemplateView):
+class IndexView(ListView):
     template_name = 'index.html'
-    
+    model = Chirp
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs) #you have to have this line
+        context['amount'] = Chirp.objects.all().count()
+        return context
